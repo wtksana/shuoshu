271652031d2cbb7f9e0585c2c1fa1fe13e89dc87
wtksana <496359370@qq.com>
2016-08-26 16:44:38 +0800

    manage初始化1.0

---
+++ manage/src/main/java/com/mutiTest/manage/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.mutiTest.manage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by wt on 2016/8/26.
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping(value="/index")
+    public String loginPage(Model model){
+        return  "/index";
+    }
+}
